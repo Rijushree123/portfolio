1556e7a0b8521e6363bb35c6ee3e82fb255dd188
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2025-09-14 21:54:33 +0530

    home page + hero section initial setup + tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
